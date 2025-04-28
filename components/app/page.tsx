0bd2aaee7df5846f31b88c83491d3add060e04eb
Bojana15 <mijatovicbojana5@gmail.com>
2025-04-28 11:44:22 +0200

    Očistila stari kod i dodala svoje sekcije

---
+++ components/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <main>
+        <h1>Zdravo, ja sam Bojana!</h1>
+        <p>Dobrodošli na moj portfolio sajt.</p>
+      </main>
+    )
+  }
+  
